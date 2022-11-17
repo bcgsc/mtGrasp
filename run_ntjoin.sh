f39ac09e89c478456dec7981bb936652dffeefab
Cecilia Yang <ceyang@hpce705.hpc.bcgsc.ca>
2022-11-17 14:03:45 -0800

    change ntJoin w to 100

--- run_ntjoin.sh
+++ run_ntjoin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $1
-ntJoin assemble target=$2 target_weight=1 references=$3  reference_weights='2' k=32 w=500 &> $4
+ntJoin assemble target=$2 target_weight=1 references=$3  reference_weights='2' k=32 w=100 &> $4
