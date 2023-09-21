930f525de76405bda27eeb13389add9f522a518a
Cecilia Yang <58164243+ycecilia@users.noreply.github.com>
2023-09-21 14:42:49 -0700

    Update create_references_for_ntjoin.py

--- create_references_for_ntjoin.py
+++ create_references_for_ntjoin.py
@@ -5,23 +5,21 @@
 
 import sys
 
+ntjoin_reference_weight = 2 #Refer to ntJoin README for more info: https://github.com/bcgsc/ntJoin
 input = sys.argv[1]
 output = sys.argv[2]
 config = sys.argv[3]
 ref_list = []
 with open(input, 'r') as f:
     for line in f:
-        line = line.strip()
-        // do the logic
-    for i in range(len(lines)):
-        if lines[i].startswith('>'):
+        if line[i].startswith('>'):
             header = lines[i].strip()
-            filename = header.split(' ')[0].replace('>', '') + '.fasta'
+            filename = header.split(' ')[0].strip('>') + '.fasta'
             sequence = lines[i+1].strip()
             with open(output + '/' + filename, 'w') as g:
                 g.write(header + '\n')
                 g.write(sequence + '\n')
-            ref_list.append(f'{output}/{filename},2')
+            ref_list.append(f'{output}/{filename},{ntjoin_reference_weight}')
 
 with open(config, 'w') as f:
     f.write('\n'.join(ref_list))
