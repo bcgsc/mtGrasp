6066f412dc8ff8ddf602387f91506791823a250c
Cecilia Yang <58164243+ycecilia@users.noreply.github.com>
2023-09-21 16:00:56 -0700

    Update create_references_for_ntjoin.py

--- create_references_for_ntjoin.py
+++ create_references_for_ntjoin.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-# Split a fasta file into multiple files, one per sequence and create a reference config file for ntjoin
+# Split a fasta file into multiple files, one per sequence and create a reference config file for ntJoin
 # Usage: split_fasta.py <fasta file> <output directory> <config file>
 
 import sys
+from itertools import islice
 
 ntjoin_reference_weight = 2 #Refer to ntJoin README for more info: https://github.com/bcgsc/ntJoin
 input = sys.argv[1]
@@ -11,11 +12,11 @@ output = sys.argv[2]
 config = sys.argv[3]
 ref_list = []
 with open(input, 'r') as f:
-    for line in f:
-        if line[i].startswith('>'):
-            header = lines[i].strip()
+    for i, line in enumerate(f):
+        if line.startswith('>'):
+            header = line.strip()
             filename = header.split(' ')[0].strip('>') + '.fasta'
-            sequence = lines[i+1].strip()
+            sequence = next(islice(f, 1, None), None).strip()
             with open(output + '/' + filename, 'w') as g:
                 g.write(header + '\n')
                 g.write(sequence + '\n')
