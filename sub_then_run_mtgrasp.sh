2e539768b11993c3c43c72710efbf7254a52f982
Cecilia Yang <58164243+ycecilia@users.noreply.github.com>
2023-11-03 13:53:40 -0700

    Update sub_then_run_mtgrasp.sh

--- sub_then_run_mtgrasp.sh
+++ sub_then_run_mtgrasp.sh
@@ -25,6 +25,7 @@ end_recov_p=${18}
 end_recov_sealer_k=${19}
 mismatch_allowed=${20}
 annotate=${21}
+mitos_path=${22}
 
 
 current_dir="$(pwd)/"
@@ -42,4 +43,4 @@ else
 fi && snakemake -s ${script_dir}/mtgrasp.smk --cores ${threads} -p -k \
 --config r1=${current_dir}${out_dir}/subsets/${read1_base}_${subsample}.fastq.gz r2=${current_dir}${out_dir}/subsets/${read2_base}_${subsample}.fastq.gz out_dir=${out_dir} mt_code=${mt_gen} k=${kmer} kc=${kc} ref_path=${ref_path} threads=${threads} \
 abyss_fpr=${abyss_fpr} sealer_fpr=${sealer_fpr} p=${p} sealer_k=${sealer_k} end_recov_sealer_fpr=${end_recov_sealer_fpr} \
-end_recov_p=${end_recov_p} end_recov_sealer_k=${end_recov_sealer_k} mismatch_allowed=${mismatch_allowed} annotate=${annotate} && rm ${out_dir}/subsets/*${subsample}.fastq.gz
+end_recov_p=${end_recov_p} end_recov_sealer_k=${end_recov_sealer_k} mismatch_allowed=${mismatch_allowed} annotate=${annotate} mitos_path=${mitos_path} && rm ${out_dir}/subsets/*${subsample}.fastq.gz
