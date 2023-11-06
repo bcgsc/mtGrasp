9f5c1bd32ddd46a565740fa64c1ea4b4e1475272
Cecilia Yang <58164243+ycecilia@users.noreply.github.com>
2023-11-06 09:28:58 -0800

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Run this script to ensure all required dependencies are installed for mtGrasp
+# Make sure to edit the version for new releases
+
 
 mitos_path=$1
 
@@ -11,7 +13,7 @@ mtgrasp.py -r1 ${script_directory}/SRR20078528_1M_subset_R1.fastq.gz -r2 ${scrip
 file=$(find test_out/final_output/test_out_k91_kc3/ -name '*-assembly.fa' -print -quit)
 if [[ -n "$file" ]]; then
     echo "mtGrasp executed successfully. All required dependencies are installed."
-    echo "The output files can be found in the test_out directory. The final assembly file can be found at: test_out/final_output/test_out_k91_kc3/test_out_k91_kc3.final-mtgrasp_v1.1.0-assembly.fa"
+    echo "The output files can be found in the test_out directory. The final assembly file can be found at: test_out/final_output/test_out_k91_kc3/test_out_k91_kc3.final-mtgrasp_<version>-assembly.fa"
 else
     echo "Error: mtGrasp could not run successfully due to missing dependencies. Please check the test.log file and make sure all required dependencies are installed before running the program."
 fi
