5823c176f57add16932c2d9f923c8929020f5a14
lcoombe <lauren.e.coombe@gmail.com>
2024-04-02 15:26:52 -0700

    Add macOS test

--- test/test.sh
+++ test/test.sh
@@ -13,6 +13,7 @@ mtgrasp.py -r1 ${script_directory}/SRR20078528_1M_subset_R1.fastq.gz -r2 ${scrip
 # check if final assembly fasta is generated
 file=$(find test_out/final_output/test_out_k91_kc3/ -name '*-assembly.fa' -print -quit)
 if [[ -n "$file" ]]; then
+    abyss-fac $file
     echo "mtGrasp executed successfully. All required dependencies are installed."
     echo "The output files can be found in the test_out directory. The final assembly file can be found at: test_out/final_output/test_out_k91_kc3/test_out_k91_kc3.final-mtgrasp_<version>-assembly.fa"
 else
